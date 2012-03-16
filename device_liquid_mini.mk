725bc86e589e65fe0e9028daa74b663b00835b52
Vasencheg <vasencheg@gmail.com>
2012-03-17 00:39:58 +0400

    device makefile changed

--- device_liquid_mini.mk
+++ device_liquid_mini.mk
@@ -17,7 +17,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/small_base.mk)
 
 $(call inherit-product-if-exists, vendor/acer/liquid_mini/liquid_mini-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += device/acer/liquid_mini/overlay
 
 PRODUCT_PACKAGES += \
     libcamera \
@@ -51,125 +51,125 @@ PRODUCT_COPY_FILES += \
 
 # recovery
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/root/etc/setupenv.sh:recovery/root/etc/setupenv.sh
+    device/acer/liquid_mini/recovery/root/etc/setupenv.sh:recovery/root/etc/setupenv.sh
 
 # Board-specific init
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/root/init.qcom.rc:root/init.qcom.rc \
-    $(LOCAL_PATH)/root/init.qcom.sh:root/init.qcom.sh \
-    $(LOCAL_PATH)/root/init.rc:root/init.rc \
-    $(LOCAL_PATH)/root/initlogo.rle:root/initlogo.rle\
-    $(LOCAL_PATH)/root/ueventd.rc:root/ueventd.rc \
-    $(LOCAL_PATH)/system/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
-    $(LOCAL_PATH)/system/etc/init.qcom.sdio.sh:system/etc/init.qcom.sdio.sh
+    device/acer/liquid_mini/root/init.qcom.rc:root/init.qcom.rc \
+    device/acer/liquid_mini/root/init.qcom.sh:root/init.qcom.sh \
+    device/acer/liquid_mini/root/init.rc:root/init.rc \
+    device/acer/liquid_mini/root/initlogo.rle:root/initlogo.rle\
+    device/acer/liquid_mini/root/ueventd.rc:root/ueventd.rc \
+    device/acer/liquid_mini/system/etc/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+    device/acer/liquid_mini/system/etc/init.qcom.sdio.sh:system/etc/init.qcom.sdio.sh
 
 # bin
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/bin/battery_charging:system/bin/battery_charging \
-    $(LOCAL_PATH)/system/bin/cciclean:system/bin/cciclean \
-    $(LOCAL_PATH)/system/bin/ccidag:system/bin/ccidag \
-    $(LOCAL_PATH)/system/bin/ccidevd:system/bin/ccidevd \
-    $(LOCAL_PATH)/system/bin/ccieuu:system/bin/ccieuu \
-    $(LOCAL_PATH)/system/bin/ccihwid:system/bin/ccihwid \
-    $(LOCAL_PATH)/system/bin/cci-props:system/bin/cci-props \
-    $(LOCAL_PATH)/system/bin/CKPD-daemon:system/bin/CKPD-daemon \
-    $(LOCAL_PATH)/system/bin/cnd:system/bin/cnd \
-    $(LOCAL_PATH)/system/bin/diag_klog:system/bin/diag_klog \
-    $(LOCAL_PATH)/system/bin/diag_mdlog:system/bin/diag_mdlog \
-    $(LOCAL_PATH)/system/bin/ds_fmc_appd:system/bin/ds_fmc_appd \
-    $(LOCAL_PATH)/system/bin/fmfc:system/bin/fmfc \
-    $(LOCAL_PATH)/system/bin/fmsvc:system/bin/fmsvc \
-    $(LOCAL_PATH)/system/bin/geomagneticd:system/bin/geomagneticd \
-    $(LOCAL_PATH)/system/bin/hostapd:system/bin/hostapd \
-    $(LOCAL_PATH)/system/bin/hostapd_cli:system/bin/hostapd_cli \
-    $(LOCAL_PATH)/system/bin/netmgrd:system/bin/netmgrd \
-    $(LOCAL_PATH)/system/bin/orientationd:system/bin/orientationd \
-    $(LOCAL_PATH)/system/bin/port-bridge:system/bin/port-bridge \
-    $(LOCAL_PATH)/system/bin/rmt_storage:system/bin/rmt_storage \
-    $(LOCAL_PATH)/system/bin/storage_keeper:system/bin/storage_keeper \
-    $(LOCAL_PATH)/system/bin/su:system/bin/su \
-    $(LOCAL_PATH)/system/bin/usbhub:system/bin/usbhub \
-    $(LOCAL_PATH)/system/bin/usbhub_init:system/bin/usbhub_init \
-    $(LOCAL_PATH)/system/bin/wlan_addr:system/bin/wlan_addr \
-    $(LOCAL_PATH)/system/bin/wpa_supplicant:system/bin/wpa_supplicant \
-    $(LOCAL_PATH)/system/bin/writefile:system/bin/writefile
+    device/acer/liquid_mini/system/bin/battery_charging:system/bin/battery_charging \
+    device/acer/liquid_mini/system/bin/cciclean:system/bin/cciclean \
+    device/acer/liquid_mini/system/bin/ccidag:system/bin/ccidag \
+    device/acer/liquid_mini/system/bin/ccidevd:system/bin/ccidevd \
+    device/acer/liquid_mini/system/bin/ccieuu:system/bin/ccieuu \
+    device/acer/liquid_mini/system/bin/ccihwid:system/bin/ccihwid \
+    device/acer/liquid_mini/system/bin/cci-props:system/bin/cci-props \
+    device/acer/liquid_mini/system/bin/CKPD-daemon:system/bin/CKPD-daemon \
+    device/acer/liquid_mini/system/bin/cnd:system/bin/cnd \
+    device/acer/liquid_mini/system/bin/diag_klog:system/bin/diag_klog \
+    device/acer/liquid_mini/system/bin/diag_mdlog:system/bin/diag_mdlog \
+    device/acer/liquid_mini/system/bin/ds_fmc_appd:system/bin/ds_fmc_appd \
+    device/acer/liquid_mini/system/bin/fmfc:system/bin/fmfc \
+    device/acer/liquid_mini/system/bin/fmsvc:system/bin/fmsvc \
+    device/acer/liquid_mini/system/bin/geomagneticd:system/bin/geomagneticd \
+    device/acer/liquid_mini/system/bin/hostapd:system/bin/hostapd \
+    device/acer/liquid_mini/system/bin/hostapd_cli:system/bin/hostapd_cli \
+    device/acer/liquid_mini/system/bin/netmgrd:system/bin/netmgrd \
+    device/acer/liquid_mini/system/bin/orientationd:system/bin/orientationd \
+    device/acer/liquid_mini/system/bin/port-bridge:system/bin/port-bridge \
+    device/acer/liquid_mini/system/bin/rmt_storage:system/bin/rmt_storage \
+    device/acer/liquid_mini/system/bin/storage_keeper:system/bin/storage_keeper \
+    device/acer/liquid_mini/system/bin/su:system/bin/su \
+    device/acer/liquid_mini/system/bin/usbhub:system/bin/usbhub \
+    device/acer/liquid_mini/system/bin/usbhub_init:system/bin/usbhub_init \
+    device/acer/liquid_mini/system/bin/wlan_addr:system/bin/wlan_addr \
+    device/acer/liquid_mini/system/bin/wpa_supplicant:system/bin/wpa_supplicant \
+    device/acer/liquid_mini/system/bin/writefile:system/bin/writefile
 
 # SD Card
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/vold.fstab:system/etc/vold.fstab
+    device/acer/liquid_mini/system/etc/vold.fstab:system/etc/vold.fstab
 
 # The gps config appropriate for this device
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/gps.conf:system/etc/gps.conf
+    device/acer/liquid_mini/system/etc/gps.conf:system/etc/gps.conf
 
 # misc
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/01_qcomm_omx.cfg:system/etc/01_qcomm_omx.cfg \
-    $(LOCAL_PATH)/system/etc/an32155a.cfg:system/etc/an32155a.cfg \
-    $(LOCAL_PATH)/system/etc/cci-flex.sh:system/etc/cci-flex.sh \
-    $(LOCAL_PATH)/system/etc/pvplayer.cfg:system/etc/pvplayer.cfg \
-    $(LOCAL_PATH)/system/etc/rtecdc.bin:system/etc/rtecdc.bin \
-    $(LOCAL_PATH)/system/etc/rtecdc_ap.bin:system/etc/rtecdc_ap.bin \
-    $(LOCAL_PATH)/system/etc/rtecdc_wapi.bin:system/etc/rtecdc_wapi.bin \
-    $(LOCAL_PATH)/system/etc/voVidDec.dat:system/etc/voVidDec.dat \
-    $(LOCAL_PATH)/system/etc/vommcodec.cfg:system/etc/vommcodec.cfg \
-    $(LOCAL_PATH)/system/etc/vosfPlay.cfg:system/etc/vosfPlay.cfg \
-    $(LOCAL_PATH)/system/ts.conf:system/ts.conf \
-    $(LOCAL_PATH)/system/usr/keychars/surf_keypad.kcm.bin:system/usr/keychars/surf_keypad.kcm.bin \
-    $(LOCAL_PATH)/system/usr/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
-    $(LOCAL_PATH)/system/usr/keylayout/surf_keypad.kl:system/usr/keylayout/surf_keypad.kl
+    device/acer/liquid_mini/system/etc/01_qcomm_omx.cfg:system/etc/01_qcomm_omx.cfg \
+    device/acer/liquid_mini/system/etc/an32155a.cfg:system/etc/an32155a.cfg \
+    device/acer/liquid_mini/system/etc/cci-flex.sh:system/etc/cci-flex.sh \
+    device/acer/liquid_mini/system/etc/pvplayer.cfg:system/etc/pvplayer.cfg \
+    device/acer/liquid_mini/system/etc/rtecdc.bin:system/etc/rtecdc.bin \
+    device/acer/liquid_mini/system/etc/rtecdc_ap.bin:system/etc/rtecdc_ap.bin \
+    device/acer/liquid_mini/system/etc/rtecdc_wapi.bin:system/etc/rtecdc_wapi.bin \
+    device/acer/liquid_mini/system/etc/voVidDec.dat:system/etc/voVidDec.dat \
+    device/acer/liquid_mini/system/etc/vommcodec.cfg:system/etc/vommcodec.cfg \
+    device/acer/liquid_mini/system/etc/vosfPlay.cfg:system/etc/vosfPlay.cfg \
+    device/acer/liquid_mini/system/ts.conf:system/ts.conf \
+    device/acer/liquid_mini/system/usr/keychars/surf_keypad.kcm.bin:system/usr/keychars/surf_keypad.kcm.bin \
+    device/acer/liquid_mini/system/usr/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
+    device/acer/liquid_mini/system/usr/keylayout/surf_keypad.kl:system/usr/keylayout/surf_keypad.kl
 
 # audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/AudioFilter.csv:system/etc/AudioFilter.csv
+    device/acer/liquid_mini/system/etc/AudioFilter.csv:system/etc/AudioFilter.csv
 
 # wifi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/nvram.txt:system/etc/nvram.txt \
-    $(LOCAL_PATH)/system/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
-    $(LOCAL_PATH)/system/lib/modules/bcm4329.ko:system/lib/modules/bcm4329.ko
+    device/acer/liquid_mini/system/etc/nvram.txt:system/etc/nvram.txt \
+    device/acer/liquid_mini/system/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/acer/liquid_mini/system/lib/modules/bcm4329.ko:system/lib/modules/bcm4329.ko
 
 # bluetooth
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/bt_fw.hcd:system/etc/bt_fw.hcd \
-    $(LOCAL_PATH)/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-    $(LOCAL_PATH)/system/etc/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
-    $(LOCAL_PATH)/system/etc/bluetooth/audio.conf:system/etc/bluetooth/audio.conf \
-    $(LOCAL_PATH)/system/etc/bluetooth/auto_pairing.conf:system/etc/bluetooth/auto_pairing.conf \
-    $(LOCAL_PATH)/system/etc/bluetooth/blacklist.conf:system/etc/bluetooth/blacklist.conf \
-    $(LOCAL_PATH)/system/etc/bluetooth/input.conf:system/etc/bluetooth/input.conf \
-    $(LOCAL_PATH)/system/etc/bluetooth/main.conf:system/etc/bluetooth/main.conf
+    device/acer/liquid_mini/system/etc/bt_fw.hcd:system/etc/bt_fw.hcd \
+    device/acer/liquid_mini/system/etc/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    device/acer/liquid_mini/system/etc/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
+    device/acer/liquid_mini/system/etc/bluetooth/audio.conf:system/etc/bluetooth/audio.conf \
+    device/acer/liquid_mini/system/etc/bluetooth/auto_pairing.conf:system/etc/bluetooth/auto_pairing.conf \
+    device/acer/liquid_mini/system/etc/bluetooth/blacklist.conf:system/etc/bluetooth/blacklist.conf \
+    device/acer/liquid_mini/system/etc/bluetooth/input.conf:system/etc/bluetooth/input.conf \
+    device/acer/liquid_mini/system/etc/bluetooth/main.conf:system/etc/bluetooth/main.conf
 
 # fm
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh
+    device/acer/liquid_mini/system/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh
 
 # permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    $(LOCAL_PATH)/system/etc/permissions/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
-    $(LOCAL_PATH)/system/etc/permissions/com.acer.android.os.xml:system/etc/permissions/com.acer.android.os.xml \
-    $(LOCAL_PATH)/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
-    $(LOCAL_PATH)/system/etc/permissions/com.qualcomm.location.vzw_library.xml:system/etc/permissions/com.qualcomm.location.vzw_library.xml \
-    $(LOCAL_PATH)/system/etc/permissions/platform.xml:system/etc/permissions/platform.xml
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    device/acer/liquid_mini/system/etc/permissions/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    device/acer/liquid_mini/system/etc/permissions/com.acer.android.os.xml:system/etc/permissions/com.acer.android.os.xml \
+    device/acer/liquid_mini/system/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+    device/acer/liquid_mini/system/etc/permissions/com.qualcomm.location.vzw_library.xml:system/etc/permissions/com.qualcomm.location.vzw_library.xml \
+    device/acer/liquid_mini/system/etc/permissions/platform.xml:system/etc/permissions/platform.xml
 
 # boot animation
 PRODUCT_COPY_FILES +=  \
-    $(LOCAL_PATH)/media/bootanimation.zip:system/media/bootanimation.zip
+    device/acer/liquid_mini/media/bootanimation.zip:system/media/bootanimation.zip
 
 # Inherit some common cyanogenmod stuff.
 #$(call inherit-product, vendor/cyanogen/products/common_full.mk)
@@ -194,8 +194,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 endif
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/system/lib/libril-qc-1.so \
-    rild.libargs=-d /dev/smd0 \
     ro.ril.hsxpa=1 \
     ro.ril.gprsclass=10 \
     ro.ril.disable.power.collapse=0 \
@@ -227,5 +225,5 @@ PRODUCT_MODEL := E310
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := LiquidMini
-PRODUCT_VERSION_DEVICE_SPECIFIC := AdvantageMX
+PRODUCT_VERSION_DEVICE_SPECIFIC := _AdvantageMX_
 -include vendor/cyanogen/products/common_versions.mk
